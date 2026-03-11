b90cd438fd6eebd3c4bf0650a42e264556268764
JoezerGoncalves <joezer.goncalves@outlook.com>
2026-03-10 21:11:37 -0300

    Add files via upload

--- 1021 - Notas e Moedas.c
+++ 1021 - Notas e Moedas.c
@@ -17,13 +17,80 @@ Obs: Utilize ponto (.) para separar a parte decimal.
 */
 
 #include <stdio.h>
+#include <stdlib.h>
  
 int main() {
  
-    float e;
-    int resto, quantnotas;
+    int qnotas, entrada, resto;
 	
-	scanf("%lf.2", e);    
+	//Entrada do valor
+	scanf("%d", &entrada);
+	
+	//Converte em valor digitado em centavos
+	entrada = entrada * 100 + 0.5;
+	
+	//Quantidade notas de R$ 100
+	qnotas = entrada / 10000;
+	resto = entrada % 10000;
+	printf("NOTAS:/n%d nota(s) de R$ 100.00\n", qnotas);
+	
+	//Quantidade notas de R$ 50
+	qnotas = resto / 5000;
+	resto = resto % 5000;
+	printf("%d nota(s) de R$ 50.00\n", qnotas);
+	
+	//Quantidade notas de R$ 20
+	qnotas = resto / 2000;
+	resto = resto % 2000;
+	printf("%d nota(s) de R$ 20.00\n", qnotas);	
+
+	//Quantidade notas de R$ 10
+	qnotas = resto / 1000;
+	resto = resto % 1000;
+	printf("%d nota(s) de R$ 10.00\n", qnotas);
+	
+	//Quantidade notas de R$ 5
+	qnotas = resto / 500;
+	resto = resto % 500;
+	printf("%d nota(s) de R$ 5.00\n", qnotas);
+	
+	//Quantidade notas de R$ 2
+	qnotas = resto / 200;
+	resto = resto % 200;
+	printf("%d nota(s) de R$ 2.00\n", qnotas);
+	
+	//Quantidade moedas
+
+	//Quantidade moedas de R$ 1.00
+	qnotas = resto / 100;
+	resto = resto % 100;
+	printf("MOEDAS:\n%d moeda(s) de R$ 1.00\n", qnotas);	
+
+	//Quantidade moedas de R$ 0.50
+	qnotas = resto / 50;
+	resto = resto % 50;
+	printf("%d moeda(s) de R$ 0.50\n", qnotas);			
  
+ 	//Quantidade moedas de R$ 0.25
+	qnotas = resto / 25;
+	resto = resto % 25;
+	printf("%d moeda(s) de R$ 0.25\n", qnotas);
+	
+ 	//Quantidade moedas de R$ 0.10
+	qnotas = resto / 10;
+	resto = resto % 10;
+	printf("%d moeda(s) de R$ 0.10\n", qnotas);
+
+ 	//Quantidade moedas de R$ 0.05
+	qnotas = resto / 05;
+	resto = resto % 05;
+	printf("%d moeda(s) de R$ 0.05\n", qnotas);
+	
+ 	//Quantidade moedas de R$ 0.01
+	qnotas = resto / 01;
+	resto = resto % 01;
+	printf("%d moeda(s) de R$ 0.01\n", qnotas);		
+	
+		
     return 0;
 }
