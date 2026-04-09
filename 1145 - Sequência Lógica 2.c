50e92f1d410eb71dc163e4e0eff1d67734174d2f
JoezerGoncalves <joezer.goncalves@outlook.com>
2026-04-08 22:09:01 -0300

    Add files via upload

---
+++ 1145 - Sequência Lógica 2.c
@@ -0,0 +1,44 @@
+/*
+beecrowd | 1145 - Sequência Lógica 2
+Adaptado por Neilor Tonin, URI  Brasil
+
+Timelimit: 1
+Escreva um programa que leia dois valores X e Y. A seguir, mostre uma sequência de 1 até Y, passando para a próxima linha a cada X números.
+
+Entrada
+O arquivo de entrada contém dois valores inteiros, (1 < X < 20) e (X < Y < 100000).
+
+Saída
+Cada sequência deve ser impressa em uma linha apenas, com 1 espaço em branco entre cada número, conforme exemplo abaixo. Não deve haver espaço em branco após o último valor da linha.
+
+Exemplo de Entrada	Exemplo de Saída
+3 99				1 2 3
+					4 5 6
+					7 8 9
+					10 11 12
+					...
+					97 98 99
+*/
+#include <stdio.h>
+ 
+int main() {
+	
+	int x, y, cont = 1, linha = 1;
+	
+	scanf("%d%d", &x, &y);	
+	
+	while(cont <= y){
+		
+		while(linha <=x){
+		printf("%d", cont);
+		printf(" ");
+		cont ++;
+		linha ++;
+		}
+	linha = 1;	
+	printf("\n");
+	}
+
+ 
+    return 0;
+}
